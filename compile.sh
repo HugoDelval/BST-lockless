b2ec50b9c199c6e45d3f0fde689e025ba43d891d
HugoDelval <hugodelval@gmail.com>
2017-01-16 14:23:01 +0000

    adding support for tcmalloc on malbec

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+g++ /users/ugrad/delvalh/BST/gperftools/lib/libtcmalloc.so helper.cpp main.cpp -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -pthread -o bst-with-tcmalloc -mrtm -mrdrnd
 g++ helper.cpp main.cpp -pthread -o bst -mrtm -mrdrnd
---
+++ run-bst-with-tcmalloc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LD_LIBRARY_PATH=/users/ugrad/delvalh/BST/gperftools/lib/ ./bst-with-tcmalloc
